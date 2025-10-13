564c9b76d4b66a2edc42a9b748508cf345b133fb
Nguhi <nguhi.dev@gmail.com>
2025-10-13 21:59:29 +0300

    feat(ui): add header component and enable routing - Implement Header component using React Bootstrap - Add routing support with React Router-

--- Client/Binge-Buddy-Client/src/App.jsx
+++ Client/Binge-Buddy-Client/src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Home from "./components/home/Home.jsx";
+import Header from "./components/header/Header.jsx";
 
 function App() {
 
 
   return (
       <>
+          <Header/>
           <Home/>
       </>
   )
